009b80561d89562611d3173aec9fd3455e8a3f01
Pedro Lucas <79203933+pedrocslucas@users.noreply.github.com>
2024-07-06 20:26:27 -0300

    removendo excessos no código

--- main.py
+++ main.py
@@ -1,37 +1,27 @@
-# Tira mensagens de alerta e informativos do tensorflow na hora da exec do cod:
+#importação das bibliotecas:
+from deepface import DeepFace
 import os
 import logging
 
+# Tira mensagens de alerta e informativos do tensorflow na hora da exec do cod:
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0' 
 logging.getLogger('tensorflow').setLevel(logging.ERROR)
 
-#importação das bibliotecas:
-from deepface import DeepFace
-import cv2
-import matplotlib.pyplot as plt
-
 #Começo do Codigo/função:
 def verify(img1_path, img2_path):
-    img1 = cv2.imread(img1_path)
-    img2 = cv2.imread(img2_path)
-    
-    #Deixando as imgs na cor certa
-    plt.imshow(img1[:, :, ::-1])
-    plt.imshow(img2[:, :, ::-1])
-
     resultado = DeepFace.verify(img1_path, img2_path)
     
     verificacao = resultado['verified']
     time_taken = resultado['time']
     
-    print("Mesma Rosto: ", verificacao)
+    print("Mesmo Rosto: ", verificacao)
     print("Tempo de Verificação: ", time_taken)
     
     if verificacao:
-        print("São os mesmos na fotos")
+        print("Carimbado =)")
     else:
-        print("Não são os mesmos na fotos!")
+        print("Incapaz =(")
 
 #Chamando a função e passando os rostos:
-verify("rostos_img/bh.jpg", "rostos_img/gab2.jpeg")
+verify("rostos_img/foto-padrao.jpeg", "rostos_img/foto-escura.jpeg")
